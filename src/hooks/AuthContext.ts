28ec437e2471263a009ccc16dde1a9d3c5f20142
MayaraPereira17 <mayaracapereira45@gmail.com>
2025-10-13 13:56:01 -0300

    integração da autenticaçãO

---
+++ src/hooks/AuthContext.ts
@@ -0,0 +1,5 @@
+// src/hooks/AuthContext.tsx
+import { createContext } from 'react';
+import type { AuthContextType } from '../types/auth';
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
